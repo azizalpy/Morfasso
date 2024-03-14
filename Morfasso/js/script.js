2707b14f9790abda50914a130d95e0ca7211f8e0
Aziz Alp Yolcu <128037700+azizalpy@users.noreply.github.com>
2024-03-14 21:44:25 +0300

    Add files via upload

---
+++ Morfasso/js/script.js
@@ -0,0 +1,36 @@
+document.getElementById('contactLink').addEventListener('click', function () {
+    
+    window.location.href = 'contact.html';
+});
+
+document.getElementById('homeLink').addEventListener('click', function () {
+    
+    window.location.href = 'index.html';
+});
+
+document.getElementById('foodLink').addEventListener('click', function () {
+    
+    window.location.href = 'foods.html';
+});
+
+document.getElementById('dessertLink').addEventListener('click', function () {
+    
+    window.location.href = 'dessert.html';
+});
+
+document.getElementById('drinkLink').addEventListener('click', function () {
+    
+    window.location.href = 'drinks.html';
+});
+
+document.getElementById('aboutLink').addEventListener('click', function () {
+    
+    window.location.href = 'index.html';
+});
+
+document.getElementById('galleryLink').addEventListener('click', function () {
+    
+    window.location.href = 'index.html';
+});
+
+
