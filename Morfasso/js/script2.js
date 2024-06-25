3b7e9a3dfd589d2691da1cd1d4a06a67721b3519
Aziz Alp Yolcu <128037700+azizalpy@users.noreply.github.com>
2024-06-25 15:49:32 +0300

    Add files via upload

---
+++ Morfasso/js/script2.js
@@ -0,0 +1,24 @@
+document.getElementById('contactLink').addEventListener('click', function () {
+    
+    window.location.href = 'contact.html';
+});
+
+document.getElementById('homeLink').addEventListener('click', function () {
+    
+    window.location.href = 'index.html';
+});
+
+document.getElementById('foodLink').addEventListener('click', function () {
+    
+    window.location.href = 'foods.html';
+});
+
+document.getElementById('dessertLink').addEventListener('click', function () {
+    
+    window.location.href = 'dessert.html';
+});
+
+document.getElementById('drinkLink').addEventListener('click', function () {
+    
+    window.location.href = 'drinks.html';
+});
